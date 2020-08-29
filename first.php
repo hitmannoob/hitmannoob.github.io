bbf377a89df40a86cb79dc3d5869898b26f36c96
hitmannoob <65114691+hitmannoob@users.noreply.github.com>
2020-08-29 16:33:16 +0530

    Add files via upload

---
+++ first.php
@@ -0,0 +1,10 @@
+<h1>This is php</h1>
+<p>
+	<?php
+		echo "hello world";
+		$answer=6*7;
+		echo "Thr answer is $answer, what ";
+		echo "What was the question again/n";
+	?>
+</p>
+<p>Yes another paragarph</p>
